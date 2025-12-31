e50f9d4d8cb7acf27c849335b47e25675600f633
Dontamsetty-Tribhuvan <tribhuvan.studyforlife@gmail.com>
2025-12-31 22:09:54 +0530

    Add distributed training setup and cleanup functions

---
+++ training/distributed.py
@@ -0,0 +1,9 @@
+import torch
+import torch.distributed as dist
+
+def setup():
+    dist.init_process_group("nccl")
+    torch.cuda.set_device(dist.get_rank())
+
+def cleanup():
+    dist.destroy_process_group()
